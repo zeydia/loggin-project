0a132f6a3d81575e75341a52d1cfc7147974827b
Zeydia <douseydiallo2002@gmail.com>
2025-02-19 23:27:44 +0000

    updating the README

---
+++ README.md
@@ -0,0 +1,13 @@
+# loggin-project
+A Beginner Login project writing in React and Spring Boot
+
+## front-end
+- Reactjs
+- BootStrap
+- Material Design BootStrap
+- React-router-dom
+
+## back-end
+- Java Spring Boot
+- Spring Security
+- MySQL
--- src/main/java/com/elijah/loggin_project/config/security/SecurityConfig.java
+++ src/main/java/com/elijah/loggin_project/config/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.elijah.loggin_project.config.security;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,12 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
